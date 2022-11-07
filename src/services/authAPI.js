be06a0dbc5b90833a6433117c51be673f8fb9d13
Mark M Aziz <mark.aziz.magdy@gmail.com>
2022-11-07 19:52:05 +0200

    register function done!

---
+++ src/services/authAPI.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export const instance = axios.create({
+  baseURL: 'http://localhost:8080/',
+});
+// register
+export function registerUser(userDate) {
+  return instance({
+    url: `auth/signup`,
+    method: 'POST',
+    data: userDate,
+  })
+    .then((response) => response)
+    .catch((err) => alert(err));
+}
