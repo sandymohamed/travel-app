713d9dac99063fad8b03d62fc57b095e5e147ec4
Mark M Aziz <mark.aziz.magdy@gmail.com>
2022-11-07 17:21:30 +0200

    some pugs paths

--- src/Components/Pages/TourGuiding/TourGuiding.js
+++ src/Components/Pages/TourGuiding/TourGuiding.js
@@ -1,19 +1,20 @@
-import './tourGuiding.scss'
+import './tourGuiding.scss';
 
 //// Header Data ////
-import headerimg from "../../../assets/header/tourGuidHeader.png"
-import HeaderComponent from '../../Shared/header/HeaderComponent'
-const headerTitle = <>Select Your tooor </>
-const headerParagraph = <> Ana msh mn sharm , </>
+import headerimg from '../../../Assets/header/tourGuidHeader.png';
+import HeaderComponent from '../../Shared/header/HeaderComponent';
+const headerTitle = <>Select Your tooor </>;
+const headerParagraph = <> Ana msh mn sharm , </>;
 function TourGuiding() {
-
-    return (
-        <>
-            <HeaderComponent img={headerimg} title={headerTitle} paragraph={headerParagraph} />
-        </>
-
-
-    )
+  return (
+    <>
+      <HeaderComponent
+        img={headerimg}
+        title={headerTitle}
+        paragraph={headerParagraph}
+      />
+    </>
+  );
 }
 
-export default TourGuiding
+export default TourGuiding;
